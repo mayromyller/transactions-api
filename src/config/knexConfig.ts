3bf8b2c1d8742e4822e104c7f3929124935c28e7
mayromyller <deehmyller11@gmail.com>
2024-09-19 13:54:11 -0300

    chore: add dotenv

--- src/config/knexConfig.ts
+++ src/config/knexConfig.ts
@@ -1,9 +1,15 @@
+import 'dotenv/config'
+
 import { knex as knexInstance, type Knex } from 'knex'
+
+if(!process.env.DATABASE_URL) {
+	throw new Error('DATABASE_URL dot env variable is not supported')
+}
 
 export const knexConfig: Knex.Config = {
 	client: 'sqlite',
 	connection: {
-		filename: './database/app.db'
+		filename: process.env.DATABASE_URL
 	},
 	migrations: {
 		extension: 'ts',
