3ffa2de0bfa0adbedc653369bf4b10d81bef8e57
mayromyller <deehmyller11@gmail.com>
2024-12-02 16:18:52 -0300

    chore: add .env.test file and update env config for tests

--- src/app/routes/__tests__/transactions.test.ts
+++ src/app/routes/__tests__/transactions.test.ts
@@ -1,4 +1,6 @@
-import { it, describe, beforeAll, afterAll, expect } from 'vitest'
+import { it, describe, beforeAll, afterAll, expect, beforeEach } from 'vitest'
+
+import { execSync } from 'node:child_process'
 
 import supertest from 'supertest'
 import { app } from '../../../app'
@@ -10,6 +12,11 @@ describe('Transactions routes', () => {
 
 	afterAll(async () => {
 		await app.close()
+	})
+
+	beforeEach(() => {
+		execSync('npx knex migrate:rollback --all')
+		execSync('npx knex migrate:latest')
 	})
 
 	it('should be able to create a new transaction', async () => {
@@ -43,5 +50,58 @@ describe('Transactions routes', () => {
 				amount: 100
 			})
 		])
+	})
+
+	it('should be able to get the summary', async () => {
+		const createTransactionResponse = await supertest(app.server).post('/transactions').send({
+			title: 'Credit transaction',
+			amount: 100,
+			type: 'credit'
+		})
+
+		const cookieResponse = createTransactionResponse.get('Set-Cookie') as string[]
+
+		await supertest(app.server).post('/transactions').set('Cookie', cookieResponse).send({
+			title: 'debit transaction',
+			amount: 50,
+			type: 'debit'
+		})
+
+		const summaryResponse = await supertest(app.server)
+			.get('/transactions/summary')
+			.set('Cookie', cookieResponse)
+			.expect(200)
+
+		expect(summaryResponse.body.summary).toEqual({
+			amount: 50
+		})
+	})
+	it('should be able to list a specific transaction', async () => {
+		const createTransactionResponse = await supertest(app.server).post('/transactions').send({
+			title: 'New transaction',
+			amount: 100,
+			type: 'credit'
+		})
+
+		const cookieResponse = createTransactionResponse.get('Set-Cookie') as string[]
+
+		const listTransactionsResponse = await supertest(app.server)
+			.get('/transactions')
+			.set('Cookie', cookieResponse)
+			.expect(200)
+
+		const transactionId = listTransactionsResponse.body.transactions[0].id
+
+		const getTransactionResponse = await supertest(app.server)
+			.get(`/transactions/${transactionId}`)
+			.set('Cookie', cookieResponse)
+			.expect(200)
+
+		expect(getTransactionResponse.body.transaction).toEqual(
+			expect.objectContaining({
+				title: 'New transaction',
+				amount: 100
+			})
+		)
 	})
 })
